a68d313e7ae8e1e1009a36a1334c3963ed7ae8d1
AlirieGray <alirie.gray@gmail.com>
2022-01-13 14:37:01 -0800

    load pdf in componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { PDFDocument } from 'pdf-lib'
+import sample from './sample.pdf'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: "",
+      width: 0,
+    }
+  }
+
+  componentDidMount = async () => {
+    let currentComponent = this;
+    await fetch(sample).then(
+      function(res){
+        return res.arrayBuffer()
+      }).then(function(data) {
+        console.log("got data??")
+        currentComponent.setState(data)
+
+
+     
+
+
+        console.log(data)
+      }).catch(
+        function(err) {
+          console.log(err)
+        }
+      )
+
+      
+      
+
+    }
+
+
+
+    // console.log("getting pdf doc")
+    // const pdfDoc = await PDFDocument.load(this.state.data)
+    // const pages = pdfDoc.getPages()
+    // const firstPage = pages[0]
+    // const { width, height } = firstPage.getSize()
+    // console.log("width ")
+    // console.log(width)
+    // currentComponent.setState(width)
+    
+
+  render() {
+    return (
+      <div>
+        {console.log("data: ", this.state.data)}
+      </div>
+    )
+  }
+
+    
+
+
 }
 
-export default App;
+export default App
